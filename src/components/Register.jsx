d48c2a8fe8b00d306c5381894c2c71f72378c324
jpswt <joel_swt@yahoo.com>
2022-10-04 21:07:53 -0500

    phone placeholder

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -88,6 +88,7 @@ function Register() {
 						id="phone"
 						type="tel"
 						label="Phone Number"
+						placeholder="555-555-5555"
 						name="phone"
 						pattern="[0-9]{10}"
 						variant="outlined"
